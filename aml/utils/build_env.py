90e95848d173440cd8c166f0a51a633ce4c9a507
Erik Zwiefel <ezwiefel@users.noreply.github.com>
2020-12-18 12:12:08 -0600

    Updates from CLI Method (#2)
    * Refactors codebase
    * Fixes issue with run config
    * Refactoring for lint

--- aml/utils/build_env.py
+++ aml/utils/build_env.py
@@ -1,7 +1,8 @@
+import sys
 from argparse import ArgumentParser, Namespace
-from azureml.core import Workspace, Environment
 from time import sleep
-from cryptography.hazmat.primitives import kdf
+
+from azureml.core import Environment, Workspace
 from docker.client import DockerClient
 from docker.models.images import Image
 
@@ -9,13 +10,14 @@ ws = Workspace.from_config()
 
 
 def parse_args() -> Namespace:
+    """Parse command line arguments"""
     parser = ArgumentParser()
     parser.add_argument("--name", '-n', type=str, required=True,
                         help="The name of the registered environment to build")
     parser.add_argument("--version", '-v', type=int,
                         help="The version of the registered environment to build",
                         default=None)
-    parser.add_argument("--async", "-q", action="store_true",
+    parser.add_argument("--async", "-q", name="run_async", action="store_true",
                         help="Run asyncronously")
     parser.add_argument('--local', '-l', action="store_true",
                         help="Build the environment on the local compute context")
@@ -39,7 +41,7 @@ def pull_image(img_name: str, registry_details: dict) -> None:
 def parse_image_details(environ: Environment) -> dict:
     """
     Pull out the image name and registry details from the environment object
-    
+
     Since the docker.login method expects 'registry' instead of 'address', rename 'address' to 'registry'
     """
     docker_details = environ.get_image_details(ws)['dockerImage']
@@ -49,11 +51,15 @@ def parse_image_details(environ: Environment) -> dict:
 
 
 def is_environment_built(environ: Environment) -> bool:
+    """
+    Checks if the environment is already built in Azure Machine Learning
+    and stored in a container registry
+    """
     env_img_details = environ.get_image_details(ws)
     return env_img_details['imageExistsInRegistry']
 
 
-def main(name: str, version: int, async: bool, local: bool, force: bool):
+def main(name: str, version: int, run_async: bool, local: bool, force: bool):
     """If the environment is already built, either exit or pull it locally"""
     environ = Environment.get(ws, name=name, version=version)
 
@@ -62,23 +68,22 @@ def main(name: str, version: int, async: bool, local: bool, force: bool):
             print(
                 f"'{name}' is already built - pulling locally. Use '--force' to force a rebuild on the local context")
             pull_image(**parse_image_details(environ))
-            exit(0)
+            sys.exit(0)
         else:
             print(f"'{name}' is already built. Use '--force' to force a rebuild")
-            exit(0)
+            sys.exit(0)
     else:
         if local:
-            build = environ.build_local(ws)
+            environ.build_local(ws)
         else:
             build = environ.build(ws)
-
-        if not async:
-            sleep(5)
-            build.wait_for_completion(show_output=True)
+            if not run_async:
+                sleep(5)
+                build.wait_for_completion(show_output=True)
 
 
 if __name__ == "__main__":
 
     args = parse_args()
     main(name=args.name, version=args.version,
-         async=args.async, local=args.local, force=args.force)
+         run_async=args.run_async, local=args.local, force=args.force)
--- code/train/train.py
+++ code/train/train.py
@@ -1,15 +1,18 @@
-import mlflow
 from argparse import ArgumentParser
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+import mlflow
 import pandas as pd
 import xgboost as xgb
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
 
 def parse_args():
+    """Parse command line arguments"""
     parser = ArgumentParser()
-    parser.add_argument("--data-path", type=str, 
+    parser.add_argument("--data-path", type=str,
                         required=True, help="The path to read the data files from")
-    parser.add_argument("--random-seed", type=int, 
+    parser.add_argument("--random-seed", type=int,
                         default=42, help="The random seed to use for data splitting and XGBoost")
     parser.add_argument('--colsample-bytree', type=float, default=1.0,
                         help='subsample ratio of columns when constructing each tree (default: 1.0)')
@@ -17,14 +20,17 @@ def parse_args():
                         help='subsample ratio of the training instances (default: 1.0)')
     return parser.parse_args()
 
+
 def main(data_path, random_seed, colsample_bytree, subsample):
-    with mlflow.start_run() as run:
+    """Main Training Loop"""
+    with mlflow.start_run():
         df = pd.read_parquet(data_path)
 
         X = df.copy()
         y = pd.DataFrame(X.pop('target'))
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_seed)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=random_seed)
 
         dtrain = xgb.DMatrix(X_train, label=y_train)
         dtest = xgb.DMatrix(X_test, label=y_test)
@@ -33,11 +39,11 @@ def main(data_path, random_seed, colsample_bytree, subsample):
         params = {
             'objective': 'reg:squarederror',
             'eval_metric': 'mlogloss',
-            'colsample_bytree': args.colsample_bytree,
-            'subsample': args.subsample,
+            'colsample_bytree': colsample_bytree,
+            'subsample': subsample,
             'seed': random_seed,
         }
-        
+
         mlflow.xgboost.autolog()
         model = xgb.train(params, dtrain)
 
@@ -48,12 +54,12 @@ def main(data_path, random_seed, colsample_bytree, subsample):
         r2 = r2_score(y_test, y_pred)
 
         mlflow.log_metrics({'val_r2': r2, 'val_rmse': rmse, 'val_mae': mae})
-    
+
+
 if __name__ == "__main__":
     args = parse_args()
-    main(data_path=args.data_path, 
+    main(data_path=args.data_path,
          random_seed=args.random_seed,
          subsample=args.subsample,
          colsample_bytree=args.colsample_bytree
-        )
-    
+         )
